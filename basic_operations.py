15eea2c5a3ee10fa3a9d9f3d7df60217a1579b3c
Joseph <musilijose618@gmail.com>
2024-12-14 11:09:27 +0300

    introduction to python

--- basic_operations.py
+++ basic_operations.py
@@ -1,9 +1,9 @@
-num1 = 10
-num2 = 5
-sum = num1 + num2
-subtarct = num2 - num1
-product = num1 * num2
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+product = number1 * number2
 
-print ("The sum of" , num1 ,"and" ,num2 , "is" ,sum)
-print ("The subtraction of" , num2 ,"and" ,num1 , "is" ,subtarct)
-print ("The product of" , num1 ,"and" ,num2 , "is" ,product)
+print ("The sum of" , number1 ,"and" ,number2 , "is" ,sum)
+print ("The subtraction of" , number1 ,"and" ,number2 , "is" ,subtract)
+print ("The product of" , number1 ,"and" ,number2 , "is" ,product)
