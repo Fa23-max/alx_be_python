cb395c6f7f158104beeb7582dec507b943140bc0
Joseph <musilijose618@gmail.com>
2024-12-21 11:55:27 +0300

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,29 @@
 task_description = input("Enter your task:")
-priority = (input("Priority (high/medium/low):")).lower()
-task_time_bound = (input("Is it time-bound? (yes/no):")).lower()
+task_priority = (input("Priority (high/medium/low):")).lower()
+time_bound = (input("Is it time-bound? (yes/no):")).lower()
 
 if(len(task_description) < 1):
   print("Input Task Description")
 else:
   match priority:
     case "high":
-      if task_time_bound.find("yes") != -1:
+      if time_bound.find("yes") != -1:
         print(f"Reminder: {task_description} is a high priority task that requires immediate attention today!")
-      elif task_time_bound.find("no") != -1:
+      elif time_bound.find("no") != -1:
         print(f"Reminder: {task_description} is a high priority. Consider completing it when you have free time.")
       else:
         print("Input Time bound")
     case "medium":
-      if task_time_bound.find("yes") != -1:
+      if time_bound.find("yes") != -1:
         print(f"Reminder: {task_description} is a medium priority task that requires immediate attention today!")
-      elif task_time_bound.find("no") != -1:
+      elif time_bound.find("no") != -1:
         print(f"Reminder: {task_description} is a medium priority. Consider completing it when you have free time.")
       else:
         print("Input Time bound")
     case "low":
-      if task_time_bound.find("yes") != -1:
+      if time_bound.find("yes") != -1:
         print(f"Note: {task_description} is a low priority task. Consider completing it when you have free time.")
-      elif task_time_bound.find("no") != -1:
+      elif time_bound.find("no") != -1:
         print(f"Note: {task_description} is a low priority task. Consider completing it when you have free time.")
       else:
         print("Input Time bound")
