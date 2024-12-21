ca4b2fa2c4650b51d031e5827e803264f6a7a88d
Joseph <musilijose618@gmail.com>
2024-12-21 10:30:46 +0300

    pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+pattern = int(input("Enter the size of the pattern:"))
+# 0 1 2 3 
+# 0 1 2 3
+# 0 1 2 3
+# 0 1 2 3
+for i in range(pattern):
+    for j in range(pattern):       
+        print("*",end="")
+    print("")
+
+
+    
