1efe8e793c079890f011e71f711b365d670dc0d4
Joseph <musilijose618@gmail.com>
2025-01-18 23:01:45 +0300

    test simple

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,13 +2,19 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestCalculator(unittest.TestCase):
+
+  def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
   def test_addition(self):
-    self.assertEqual(SimpleCalculator().add(3,2),5,"3 plus 2  expected 5")
-    self.assertEqual(SimpleCalculator().add(2, 3), 5)
-    self.assertEqual(SimpleCalculator().add(-1, 1), 0)
+    self.assertEqual(self.calc.add(3,2),5,"3 plus 2  expected 5")
+    self.assertEqual(self.calc.add(2, 3), 5)
+    self.assertEqual(self.calc.add(-1, 1), 0)
   def test_subtraction(self):
-    self.assertEqual(SimpleCalculator().subtract(3,2),1,"3 subtract 2 expected 1")
+    self.assertEqual(self.calc.subtract(3,2),1,"3 subtract 2 expected 1")
   def test_multiplication(self):
-    self.assertEqual(SimpleCalculator().multiply(3,2),6,"3 multiply 2 expected 6")
+    self.assertEqual(self.calc.multiply(3,2),6,"3 multiply 2 expected 6")
   def test_division(self):
-    self.assertEqual(SimpleCalculator().divide(6,3),2,"6 divide 3 expected 2")
+    self.assertEqual(self.calc.divide(6,3),2,"6 divide 3 expected 2")
+    self.assertEqual(self.calc.divide(3,0,None,"3 divide 0 expected None"))
