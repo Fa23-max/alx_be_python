4a1d1a0ed601241e8ddfae3b57f4abe79fd0ac2d
Joseph <musilijose618@gmail.com>
2025-01-18 21:55:55 +0300

    proggraming paradigm

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR =5/9  # Factor to convert Fahrenheit to Celsius
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5  # Factor to convert Celsius to Fahrenheit
+FAHRENHEIT_TO_CELSIUS_FACTOR =5/9  
+CELSIUS_TO_FAHRENHEIT_FACTOR =9/5  
 
 def convert_to_celsius(fahrenheit):
   """Converts a temperature in Fahrenheit to Celsius and returns the result."""
