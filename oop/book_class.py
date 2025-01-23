badddb88a3e455a226b237d994f7a92b0015a48c
Joseph <musilijose618@gmail.com>
2025-01-23 13:44:44 +0300

    Books

---
+++ oop/book_class.py
@@ -0,0 +1,21 @@
+class Book:
+    def __init__(self,title,author,year):
+        self.title = title
+        self.author = author
+        self.year = year
+    def __del__(self):
+        print(f"Deleting {self.title}")
+        
+    def __str__(self):
+        return f"title : {self.title},author : {self.author},year : {self.year}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+my_book = Book("1984", "George Orwell", 1949)
+print(my_book)
+
+print(repr(my_book))
+
+del my_book
+
+
